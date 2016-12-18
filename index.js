f837c1f607736c7411843aec9372d67be4e0fa21
Megan Brown <stockbtcrew@gmail.com>
2016-12-18 11:20:58 -0700

    Add function to stringify and pass to local storage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-function NewIdea(id, title, body) {
-  this.id = id;
+function NewIdea(title, body) {
+  // this.id = id;
   this.title = title;
   this.body = body;
   this.quality = "swill";
@@ -10,11 +10,19 @@ function grabValues() {
   var title = $( ".title" ).val();
   var idea = $( ".idea" ).val();
   console.log(title + " " + idea);
-  var newIdea = new NewIdea(1, title, idea);
+  var newIdea = new NewIdea(title, idea);
   console.log(newIdea);
+  stringObj(newIdea);
 }
 
 
 $( ".save" ).on("click",function() {
   grabValues();
 });
+
+function stringObj(ideaObj) {
+  var stringObj = JSON.stringify(ideaObj);
+  localStorage.setItem(idNum, stringObj);
+  idNum ++;
+}
+var idNum = 1;
