75f3b001f02898af4b96b535ebf7e363cfe1609f
Megan Brown <stockbtcrew@gmail.com>
2016-12-18 16:28:37 -0700

    add test on blur to updatelocal storage

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ $(".save").on("click", function() {
   clearInputs();
 });
 
-function clearInputs () {
-  $(".title").val("");
-  $(".idea").val("");
-}
 $(".entries").on("click", ".delete", function() {
   var id = $(this).parent().attr("id");
   localStorage.removeItem(id);
   $(this).parent().remove();
 });
+
+// $(".entries").on("blur",".edit", function() {
+//   var updatedTitle = $("h5");
+//   var updatedIdea = $("p");
+//   console.log(updatedIdea + " " + updatedTitle);
+//   var newIdea = new NewIdea(updatedTitle, updatedIdea);
+//   console.log(newIdea);
+//   stringObj(newIdea);
+//   console.log("hi");
+// });
 
 function NewIdea(title, body) {
   this.id = uniqueID;
@@ -27,11 +33,15 @@ function NewIdea(title, body) {
   this.quality = "swill";
 }
 
+function clearInputs () {
+  $(".title").val("");
+  $(".idea").val("");
+}
+
 function grabValues() {
   var title = $( ".title" ).val();
   var idea = $( ".idea" ).val();
   var newIdea = new NewIdea(title, idea);
-  // console.log(newIdea);
   stringObj(newIdea);
 }
 
@@ -53,9 +63,9 @@ function retrieveIdeas() {
 function displayIdea(newIdeaContent) {
   $(".entries").prepend(`
     <article id="${newIdeaContent.id}" class="idea-card">
-    <h5 contenteditable>${newIdeaContent.title}</h5>
+    <h5 class="edit" contenteditable>${newIdeaContent.title}</h5>
     <img class="delete" src="images/delete.svg">
-    <p contenteditable>${newIdeaContent.body}</p>
+    <p class="edit" contenteditable>${newIdeaContent.body}</p>
     <h6>${newIdeaContent.quality}</h6>
     </article>`
   );
