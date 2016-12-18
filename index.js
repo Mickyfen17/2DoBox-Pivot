0c58db597e84aaf5e3e75e5cda73ff0b1c86b282
Megan Brown <stockbtcrew@gmail.com>
2016-12-18 11:02:15 -0700

    add object constructor function

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+
+function NewIdea(id, title, body) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.quality = "swill";
+}
+
 function grabValues() {
   var title = $( ".title" ).val();
   var idea = $( ".idea" ).val();
-  console.log(idea + " " + title);
-  // var createIdea = new CreateIdea(title, idea);
-  // ideaContent(createIdea);
+  console.log(title + " " + idea);
+  var newIdea = new NewIdea(1, title, idea);
+  console.log(newIdea);
 }
+
+
 $( ".save" ).on("click",function() {
   grabValues();
-
 });
