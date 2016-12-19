eb94bdf964c3d353f12e7c408a3b0fb8685597b8
Megan Brown <stockbtcrew@gmail.com>
2016-12-19 14:34:06 -0700

    add comments to js file

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 var uniqueID;
 
+//on load retrieve all items from local storage
 window.onload = function() {
   retrieveIdeas();
 };
-
+//event listener for save button
 $(".save").on("click", function() {
   uniqueID = Date.now();
   grabValues();
   clearInputs();
 });
-
+//deletes the idea card from DOM and storage
 $(".entries").on("click", ".delete", function() {
   var id = $(this).parent().attr("id");
   localStorage.removeItem(id);
   $(this).parent().remove();
 });
-
+//event listener for upvote
 $(".entries").on("click", ".upvote", function () {
   var thisQuality = $(this).siblings(".quality");
   upVote(thisQuality);
   updateStoredQuality(this, thisQuality);
 });
+//event listener to downvote
 $(".entries").on("click", ".downvote", function () {
   var thisQuality = $(this).siblings(".quality");
   downVote(thisQuality);
   updateStoredQuality(this, thisQuality);
 });
-function updateStoredQuality(ideaCard, newQuality) {
-  var id = $(ideaCard).parent().attr("id");
-  var itemsToEdit = JSON.parse(localStorage.getItem(id));
-  itemsToEdit.quality = newQuality.text();
-  localStorage.setItem(id, JSON.stringify(itemsToEdit));
-}
+//conditional to check and change quality upon upvote
 function upVote(currentQuality) {
   if (currentQuality.text() === "swill") {
     currentQuality.text("plausible");
@@ -39,6 +36,7 @@ function upVote(currentQuality) {
     currentQuality.text("genius");
   }
 }
+//conditional to check and change quality upon downvote
 function downVote(currentQuality) {
   if (currentQuality.text() === "genius") {
     currentQuality.text("plausible");
@@ -46,7 +44,14 @@ function downVote(currentQuality) {
     currentQuality.text("swill");
   }
 }
-
+//updates stored quality values after a vote click
+function updateStoredQuality(ideaCard, newQuality) {
+  var id = $(ideaCard).parent().attr("id");
+  var itemsToEdit = JSON.parse(localStorage.getItem(id));
+  itemsToEdit.quality = newQuality.text();
+  localStorage.setItem(id, JSON.stringify(itemsToEdit));
+}
+//event listener to edit title and update title value in local storage
 $(".entries").on("blur", "h5", function() {
   var id = $(this).parent().attr("id");
   var updatedTitle = $(this).text();
@@ -54,6 +59,7 @@ $(".entries").on("blur", "h5", function() {
   var newIdea = new NewIdea(id, updatedTitle, updatedIdea);
   stringObj(id, newIdea);
 });
+//event listener to edit body and update body value in local storage
 $(".entries").on("blur", "p", function() {
   var id = $(this).parent().attr("id");
   var updatedIdea = $(this).text();
@@ -73,31 +79,34 @@ $(".entries").on("blur", "p", function() {
 //   // stringObj(id, newIdea);
 // });
 
+
+//constructor function
 function NewIdea(id, title, body) {
   this.id = id;
   this.title = title;
   this.body = body;
   this.quality = "swill";
 }
-
+//clear input fields
 function clearInputs () {
   $(".title").val("");
   $(".idea").val("");
 }
-
+//grabs input value and assigns to new idea object
 function grabValues() {
   var title = $(".title").val();
   var idea = $(".idea").val();
   var newIdea = new NewIdea(uniqueID, title, idea);
   stringObj(uniqueID, newIdea);
 }
-
+//function to stringify new object and save to local storage
 function stringObj(id, ideaObj) {
   var stringObj = JSON.stringify(ideaObj);
   localStorage.setItem(id, stringObj);
   retrieveIdeas();
 }
 
+//removes all previous ideas and then loops through local storage and parses the data
 function retrieveIdeas() {
   $(".idea-card").remove();
   for(var key in localStorage) {
@@ -106,7 +115,7 @@ function retrieveIdeas() {
     displayIdea(parsed);
   }
 }
-
+//displays parsed Ideas to DOM//
 function displayIdea(newIdeaContent) {
   $(".entries").prepend(`
     <article id="${newIdeaContent.id}" class="idea-card">
