6f1bc0549e88ea712dc2a6cf12836682ae6d4f35
Jon Willemain <jon.willemain@gmail.com>
2017-01-25 10:52:47 -0700

    nothing

--- test/index.js
+++ test/index.js
@@ -1,7 +1,14 @@
-const assert = require('chai').assert
+describe('testing ideabox', function() {
+  let driver
+  test.beforeEach(()=> {
+  this.timeout(10000)
+  driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build()
+  driver.get('http://localhost:8080')
+  });
 
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true)
-    })
-  })
+  test.afterEach(()=>{
+    driver.quit()
+  });
+})
