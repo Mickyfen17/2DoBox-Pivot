b71371e3ea1d7204930a91dcec00f820f1e06c2e
Michael Fenwick <mickyfen17@aol.com>
2017-01-24 18:37:21 -0700

    Add second passing test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert    = require("assert");
 const webdriver = require("selenium-webdriver");
 const test      = require("selenium-webdriver/testing");
 
-describe('testing 2Do Pivot', () => {
+describe('testing 2Do Pivot', function() {
   let driver;
 
   // test.beforeEach(() => {
@@ -16,15 +16,30 @@ describe('testing 2Do Pivot', () => {
   //   driver.quit();
   // });
 
-  test.it('should allow me to input a title', () => {
+  test.it("should allow me to input a title", function() {
+    this.timeout(10000);
     const driver = new webdriver.Builder()
-                                  .forBrowser("chrome")
-                                  .build();
+                                        .forBrowser("chrome")
+                                        .build();
     driver.get("http://localhost:8080");
 
     const title = driver.findElement({className: "title"});
     title.sendKeys("Testing the title input field").then(() => title.getAttribute("value")).then((value) => {
       assert.equal(value, "Testing the title input field");
+    });
+    driver.quit();
+  });
+
+  test.it("should allow me to input a task", function() {
+    this.timeout(10000);
+    const driver = new webdriver.Builder()
+                                        .forBrowser("chrome")
+                                        .build();
+    driver.get("http://localhost:8080");
+
+    const task = driver.findElement({className: "task"});
+    task.sendKeys("Testing the task input field").then(() => task.getAttribute("value")).then((value) => {
+      assert.equal(value, "Testing the task input field");
     });
     driver.quit();
   });
