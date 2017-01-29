af1e810fd447518962b42532b5c14b2eabcc4c33
Michael Fenwick <mickyfen17@aol.com>
2017-01-28 23:04:04 -0700

    add svg-url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,17 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loader: "style!css!sass" }
+      { test: /\.scss$/, loader: "style!css!sass" },
+      { test: /\.svg$/, loader: "svg-url-loader"}
     ]
   },
   resolve: {
     extensions: ['', '.js', '.json', '.scss', '.css']
+  },
+  node: {
+    fs: "empty",
+    child_process: 'empty',
+    net: "empty",
+    readline: 'empty'
   }
-}
+};
