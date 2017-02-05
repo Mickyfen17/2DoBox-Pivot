4a361a2fbe6df846f5491bbb038a70546a9beecc
Jon Willemain <jon.willemain@gmail.com>
2017-02-05 08:56:44 -0700

    stashing stuff

--- test/test.js
+++ test/test.js
@@ -1,19 +1,21 @@
 const assert    = require("assert");
 const webdriver = require("selenium-webdriver");
 const test      = require("selenium-webdriver/testing");
+require('locus')
+
 
 describe("testing 2Do Pivot", function() {
   let driver;
 
   test.beforeEach(() => {
-    this.timeout(10000);
+    // this.timeout(10000);
     driver = new webdriver.Builder()
                           .forBrowser("chrome")
                           .build();
     driver.get("http://localhost:8080");
   });
   test.afterEach(() => {
-    driver.quit();
+    // driver.quit();
   });
 
   test.it("should allow me to input a title", function() {
@@ -49,4 +51,69 @@ describe("testing 2Do Pivot", function() {
     );
   });
 
+  test.it("should remove a todo when the user hits the delete button", function() {
+    const title = driver.findElement({className: 'title'});
+    const task  = driver.findElement({className: 'task'});
+    const save  = driver.findElement({className: 'save'});
+
+    title.sendKeys('testing a title')
+         .then(() => task.sendKeys('testing a task'))
+         .then(() => save.click());
+
+    title.sendKeys('testing another title')
+         .then(() => task.sendKeys('testing another task'))
+         .then(() => save.click());
+
+    driver.findElements({tagName: 'article'}).then((dogshit)=> {
+      assert.equal(dogshit.length, 2)
+    });
+
+    driver.findElement({className: 'delete'}).click()
+
+    driver.findElements({tagName: 'article'}).then((dogshit)=> {
+      assert.equal(dogshit.length, 1)
+    });
  });
+
+  test.it("should increase the qualityCount when the user hits upvote", function() {
+    const title = driver.findElement({className: 'title'});
+    const task  = driver.findElement({className: 'task'});
+    const save  = driver.findElement({className: 'save'});
+
+    title.sendKeys('testing the title')
+         .then(() => task.sendKeys('testing the tasks'))
+         .then(() => save.click());
+
+
+    driver.findElements({tagName: 'article'}).then((dogshit)=> {
+      assert.equal(dogshit.quality, 'Normal')
+    });
+
+    driver.findElement({className: 'upvote'}).click()
+
+    driver.findElements({tagName: 'article'}).then((dogshit)=> {
+      assert.equal(dogshit.quality, 'High')
+    });
+ });
+
+ test.it("should decrease the qualityCount when the user hits upvote", function() {
+   const title = driver.findElement({className: 'title'});
+   const task  = driver.findElement({className: 'task'});
+   const save  = driver.findElement({className: 'save'});
+
+   title.sendKeys('testing the title')
+        .then(() => task.sendKeys('testing the tasks'))
+        .then(() => save.click())
+
+   driver.findElements({tagName: 'article'}).then((dogshit)=> {
+     assert.equal(dogshit.quality, 'Normal')
+   });
+
+   driver.findElement({className: 'downvote'}).click()
+
+   driver.findElements({tagName: 'article'}).then((dogshit)=> {
+     assert.equal(dogshit.quality, 'Low')
+   });
+ });
+
+});
